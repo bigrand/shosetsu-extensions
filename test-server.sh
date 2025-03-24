3bc5a0ab311ef27c49200da468dfa5380264bd30
Bigrand <bigr4nd@gmail.com>
2025-03-23 20:10:40 -0500

    fix: clear gh-pages branch

--- test-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-# This file simply runs a basic http server locally.
-# Requires python to be installed with the http.server module.
-# Defaults to hosting on: http://localhost:8000
-
-python -m http.server 8000 --bind 0.0.0.0
